f37f13d6a2f3f6fbec8c9a0e3f2cc2d66b9c5a33
Night Sailor <sulemanshah432@gmail.com>
2021-10-22 22:11:09 +0000

    first commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">Welcome to</span>
+        <span className="headerTitleLg">Petrature</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://www.pixelstalk.net/wp-content/uploads/2016/08/Cute-Puppy-On-The-Beach-Background.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/api/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">JOIN US</span>
+        <img
+          src="https://source.unsplash.com/random?pet"
+          alt=""
+          width="236"
+        />
+        <p>
+          Welcome! Are you also a pet lover, Please share us you story?
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          {cats.map((c) => (
+            <Link to={`/?cat=${c.name}`} className="link">
+            <li className="sidebarListItem">{c.name}</li>
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
